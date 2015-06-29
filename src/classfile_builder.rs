a772712e0739c37a46aecad0403d5c5bcb759553
Ken Pratt <ken@kenpratt.net>
2015-06-29 11:40:01 -0400

    Back-ported changes from rusty_scheme.

--- src/classfile_builder.rs
+++ src/classfile_builder.rs
@@ -5,99 +5,107 @@ pub const ACC_STATIC: u16 = 0x8;
 
 pub struct ClassfileBuilder {
     access_flags: u16,
-    this_class: &'static str,
-    super_class: &'static str,
+    this_class_index: u16,
+    super_class_index: u16,
     constants: Vec<Constant>,
     methods: Vec<Method>,
 }
 
-pub struct MethodBuilder<'a> {
-    classfile: &'a mut ClassfileBuilder,
-    access_flags: u16,
-    name: &'static str,
-    descriptor: &'static str,
-    instructions: Vec<Instruction>,
-}
-
 impl ClassfileBuilder {
-    pub fn new(access_flags: u16, this_class: &'static str, super_class: &'static str) -> ClassfileBuilder {
-        ClassfileBuilder {
+    pub fn new(access_flags: u16, this_class: &str, super_class: &str) -> ClassfileBuilder {
+        let mut builder = ClassfileBuilder {
             access_flags: access_flags,
-            this_class: this_class,
-            super_class: super_class,
+            this_class_index: 0,
+            super_class_index: 0,
             constants: vec![],
             methods: vec![],
-        }
+        };
+        builder.this_class_index = builder.define_class(this_class);
+        builder.super_class_index = builder.define_class(super_class);
+        builder
     }
 
-    pub fn define_method(&mut self, access_flags: u16, name: &'static str, descriptor: &'static str) -> MethodBuilder {
+    pub fn define_method(&mut self, access_flags: u16, name: &str, descriptor: &str) -> MethodBuilder {
         MethodBuilder::new(self, access_flags, name, descriptor)
     }
 
     fn push_constant(&mut self, constant: Constant) -> u16 {
+        // TODO check if this constant is exactly equal to anything already defined in constants. If so, return the existing index instead of re-defining it.
         self.constants.push(constant);
         self.constants.len() as u16
     }
 
-    fn define_utf8(&mut self, string: &'static str) -> u16 {
+    fn define_utf8(&mut self, string: &str) -> u16 {
         self.push_constant(Constant::Utf8(string.to_owned()))
     }
 
-    fn define_class(&mut self, class: &'static str) -> u16 {
+    fn define_class(&mut self, class: &str) -> u16 {
         let name_index = self.define_utf8(class);
         self.push_constant(Constant::Class(name_index))
     }
 
-    fn define_name_and_type(&mut self, name: &'static str, descriptor: &'static str) -> u16 {
+    fn define_name_and_type(&mut self, name: &str, descriptor: &str) -> u16 {
         let name_index = self.define_utf8(name);
         let descriptor_index = self.define_utf8(descriptor);
         self.push_constant(Constant::NameAndType(name_index, descriptor_index))
     }
 
-    fn define_fieldref(&mut self, class: &'static str, name: &'static str, descriptor: &'static str) -> u16 {
+    fn define_fieldref(&mut self, class: &str, name: &str, descriptor: &str) -> u16 {
         let class_index = self.define_class(class);
         let name_and_type_index = self.define_name_and_type(name, descriptor);
         self.push_constant(Constant::Fieldref(class_index, name_and_type_index))
     }
 
-    fn define_methodref(&mut self, class: &'static str, name: &'static str, descriptor: &'static str) -> u16 {
+    fn define_methodref(&mut self, class: &str, name: &str, descriptor: &str) -> u16 {
         let class_index = self.define_class(class);
         let name_and_type_index = self.define_name_and_type(name, descriptor);
         self.push_constant(Constant::Methodref(class_index, name_and_type_index))
     }
 
-    pub fn done(mut self) -> Classfile {
-        let this_class_index = self.define_class(self.this_class);
-        let super_class_index = self.define_class(self.super_class);
-        Classfile::new(self.constants, self.access_flags, this_class_index, super_class_index, self.methods)
+    pub fn done(self) -> Classfile {
+        Classfile::new(self.constants, self.access_flags, self.this_class_index, self.super_class_index, self.methods)
     }
 }
 
+pub struct MethodBuilder<'a> {
+    classfile: &'a mut ClassfileBuilder,
+    access_flags: u16,
+    name_index: u16,
+    descriptor_index: u16,
+    instructions: Vec<Instruction>,
+    curr_stack_depth: u16,
+    max_stack_depth: u16,
+}
+
 impl<'a> MethodBuilder<'a> {
-    fn new(classfile: &'a mut ClassfileBuilder, access_flags: u16, name: &'static str, descriptor: &'static str) -> MethodBuilder<'a> {
+    fn new(classfile: &'a mut ClassfileBuilder, access_flags: u16, name: &str, descriptor: &str) -> MethodBuilder<'a> {
+        let name_index = classfile.define_utf8(name);
+        let descriptor_index = classfile.define_utf8(descriptor);
         MethodBuilder {
             classfile: classfile,
             access_flags: access_flags,
-            name: name,
-            descriptor: descriptor,
+            name_index: name_index,
+            descriptor_index: descriptor_index,
             instructions: vec![],
+            curr_stack_depth: 0,
+            max_stack_depth: 0,
         }
     }
 
-    pub fn get_static(&mut self, class: &'static str, name: &'static str, descriptor: &'static str) {
+    pub fn get_static(&mut self, class: &str, name: &str, descriptor: &str) {
         let fieldref_index = self.classfile.define_fieldref(class, name, descriptor);
         self.instructions.push(Instruction::GetStatic(fieldref_index));
         self.increase_stack_depth();
     }
 
-    pub fn invoke_virtual(&mut self, class: &'static str, name: &'static str, descriptor: &'static str) {
+    pub fn invoke_virtual(&mut self, class: &str, name: &str, descriptor: &str) {
         let methodref_index = self.classfile.define_methodref(class, name, descriptor);
         self.instructions.push(Instruction::InvokeVirtual(methodref_index));
         self.decrease_stack_depth();
     }
 
-    pub fn bipush(&mut self, value: u8) {
-        self.instructions.push(Instruction::Bipush(value));
+    pub fn bipush(&mut self, value: i8) {
+        self.instructions.push(Instruction::Bipush(value as u8));
         self.increase_stack_depth();
     }
 
@@ -111,19 +119,21 @@ impl<'a> MethodBuilder<'a> {
     }
 
     fn increase_stack_depth(&mut self) {
-        // TODO implement maximum stack size tracking
+        self.curr_stack_depth += 1;
+        if self.curr_stack_depth > self.max_stack_depth {
+            self.max_stack_depth = self.curr_stack_depth;
+        }
     }
 
     fn decrease_stack_depth(&mut self) {
-        // TODO implement maximum stack size tracking
+        self.curr_stack_depth -= 1;
     }
 
-    pub fn done(mut self) {
+    pub fn done(self) {
         let classfile = self.classfile;
-        let name_index = classfile.define_utf8(self.name);
-        let descriptor_index = classfile.define_utf8(self.descriptor);
         let code_index = classfile.define_utf8("Code");
-        let method = Method::new(self.access_flags, name_index, descriptor_index, vec![Attribute::Code(code_index, 3, 2, self.instructions, vec![], vec![])]);
+        // TODO track locals counts instead of hard-coding
+        let method = Method::new(self.access_flags, self.name_index, self.descriptor_index, vec![Attribute::Code(code_index, self.max_stack_depth, 1, self.instructions, vec![], vec![])]);
         classfile.methods.push(method);
     }
 }
