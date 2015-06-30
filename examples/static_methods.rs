2fb7f29ded7ee1985b59990321ee11dbc7c08d50
Ken Pratt <ken@kenpratt.net>
2015-06-30 19:11:48 -0400

    Added function argument to static method example.

--- examples/static_methods.rs
+++ examples/static_methods.rs
@@ -7,26 +7,36 @@ fn main() {
 
     {
         let mut method = class.define_method(ACC_PUBLIC | ACC_STATIC, "main", "([Ljava/lang/String;)V");
-        method.invoke_static("static_methods", "hello1", "()V", 0, false);
-        method.invoke_static("static_methods", "hello2", "()V", 0, false);
+        method.invoke_static("static_methods", "hello_world", "()V", 0, false);
+        method.load_constant("Rust");
+        method.invoke_static("static_methods", "hello_someone", "(Ljava/lang/String;)V", 1, false);
         method.do_return();
         method.done();
     }
 
     {
-        let mut method = class.define_method(ACC_STATIC, "hello1", "()V");
+        let mut method = class.define_method(ACC_STATIC, "hello_world", "()V");
         method.get_static("java/lang/System", "out", "Ljava/io/PrintStream;");
-        method.load_constant("Hello, World One!");
+        method.load_constant("Hello, World!");
         method.invoke_virtual("java/io/PrintStream", "println", "(Ljava/lang/String;)V", 1, false);
         method.do_return();
         method.done();
     }
 
     {
-        let mut method = class.define_method(ACC_STATIC, "hello2", "()V");
+        let mut method = class.define_method(ACC_STATIC, "hello_someone", "(Ljava/lang/String;)V");
         method.get_static("java/lang/System", "out", "Ljava/io/PrintStream;");
-        method.load_constant("Hello, World Two!");
+        method.load_constant("Hello, ");
+        method.invoke_virtual("java/io/PrintStream", "print", "(Ljava/lang/String;)V", 1, false);
+
+        method.get_static("java/lang/System", "out", "Ljava/io/PrintStream;");
+        method.aload0();
+        method.invoke_virtual("java/io/PrintStream", "print", "(Ljava/lang/String;)V", 1, false);
+
+        method.get_static("java/lang/System", "out", "Ljava/io/PrintStream;");
+        method.load_constant("!");
         method.invoke_virtual("java/io/PrintStream", "println", "(Ljava/lang/String;)V", 1, false);
+
         method.do_return();
         method.done();
     }
